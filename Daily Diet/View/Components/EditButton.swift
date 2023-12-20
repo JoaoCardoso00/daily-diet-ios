3bec6f427e3ab886a6d70984bfe7b8d9e6b8c54a
JoaoCardoso00 <jvcq13@gmail.com>
2023-12-20 18:33:42 -0300

    project setup + edit button component

---
+++ Daily Diet/View/Components/EditButton.swift
@@ -0,0 +1,31 @@
+//
+//  EditButton.swift
+//  Daily Diet
+//
+//  Created by joao cardoso on 20/12/23.
+//
+
+import SwiftUI
+
+struct EditButton: View {
+    var buttonText: String
+    var buttonAction: () -> Void
+
+    var body: some View {
+        Button(action: buttonAction) {
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundStyle(Color(.gray2))
+                    .frame(height: 65)
+                HStack {
+                    Image(systemName: "pencil.line").foregroundStyle(Color(.white)).font(.title2)
+                    Text("\(buttonText)").foregroundStyle(Color(.white)).font(.title2).fontWeight(.bold)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    EditButton(buttonText: "Label") {}
+}
