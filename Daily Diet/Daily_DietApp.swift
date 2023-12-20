6b7a292b92570575fc960c6ce5c5fdd8ccc8087e
JoaoCardoso00 <jvcq13@gmail.com>
2023-12-20 17:48:11 -0300

    Initial Commit

---
+++ Daily Diet/Daily_DietApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_DietApp.swift
+//  Daily Diet
+//
+//  Created by joao cardoso on 20/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Daily_DietApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
